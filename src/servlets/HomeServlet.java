db9124df5c8b4d562d90544eaae899b611ce76de
Justin Castronovo <jcastron@oswego.edu>
2021-02-24 18:12:53 -0500

    HW1

--- src/servlets/HomeServlet.java
+++ src/servlets/HomeServlet.java
@@ -12,7 +12,13 @@ public class HomeServlet extends HttpServlet{
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("Welcome user");
+        out.println("<!DOCTYPE html><html>");
+        out.println("<h1>Welcome to your workout tracker!</h1>");
+        out.println("<button type='submit' value='Submit'>Home</button>");
+        out.println("<button type='submit' value='Submit'>Upper Body Exercises</button>");
+        out.println("<button type='submit' value='Submit'>Lower Body Exercises</button>");
+        out.println("<button type='submit' value='Submit'>Cardio</button>");
+        out.println("</html>");
         
 	}
 }
--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -1,7 +1,7 @@
 package servlets;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -13,15 +13,27 @@ public class LoginServlet extends HttpServlet{
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
-	
-		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		
 		//Get user info from webpage
-		String firstName = request.getParameter("fname");
-		String lastName = request.getParameter("lname");
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
 		
-		RequestDispatcher rd = request.getRequestDispatcher("home");
-		rd.forward(request, response);
+		if(userName.equals("java") & password.equals("123")) {
+			//forwards the request to HomeServlet
+			RequestDispatcher rd = request.getRequestDispatcher("home");
+			rd.forward(request, response);
+		} else {
+			out.println("User Name or Password incorrect");
+			RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+			rd.include(request, response);
+		
+		}
+		
+		
+		
+	
 		
 	}
 }
